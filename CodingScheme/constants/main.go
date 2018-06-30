6c231a8673b1939d013c9c61be08c75544f05916
bif002 <igor.fld@gmail.com>
2018-06-30 11:47:12 -0400

    multiple constants declaration

--- CodingScheme/constants/main.go
+++ CodingScheme/constants/main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 )
 
-const p string = "This is constant"
+const (
+	/* The following comments are MANDATORY for linter to not
+	through warning message like
+	exported const PI should have comment (or a comment on this block) or be unexported */
+
+	// PI : define constant Pi
+	PI = 3.14
+	// Language : define language const
+	Language = "Go"
+)
 
 func main() {
 
 	const q = 42
-	fmt.Println("p - ", p)
-	fmt.Println("q - ", q)
+	fmt.Println(PI)
+	fmt.Println(Language)
 }
